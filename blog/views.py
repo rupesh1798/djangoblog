089b7756185c7a5c1ec812af8b8a02d0279ed8d2
Rupesh Mangal <rupesh.code@gmail.com>
2017-06-14 23:18:49 +0530

    passing posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 from django.shortcuts import render
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
