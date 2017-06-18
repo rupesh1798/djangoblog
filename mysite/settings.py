8a9fd930652d910f9d7334a6e7d023a930324de5
Rupesh Mangal <rupesh.code@gmail.com>
2017-06-18 12:15:52 +0530

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eg!%4yd7to3c!y!mcfbdixmuf5^jfzhllnk6$a7sdeqr+xfm%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
